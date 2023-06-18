0febd4d5c478e10886df72ddb969bf2f04cf2f4b
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-06-18 13:35:20 -0300

    req 1 - done - study_schedule func

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # raise NotImplementedError
+    if not target_time:
+        return None
 
+    student_counter = 0
 
-# inicio
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            student_counter += 1
+    return student_counter
