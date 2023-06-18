23aa645e74319aa256e6944bb78e8fec51543cc5
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-06-18 14:04:20 -0300

    req 2 - Done - adding test for encrypt_message func

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # pass
+    assert encrypt_message("teste", 3) == "set_et"
+    assert encrypt_message("teste", 2) == "ets_et"
+    assert encrypt_message("", 2) == ""
+    with pytest.raises(TypeError):
+        encrypt_message(2, 2)
+    with pytest.raises(TypeError):
+        encrypt_message("teste", "teste")
