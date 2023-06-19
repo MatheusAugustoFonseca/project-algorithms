1ad0b23f421bf07cb878810bae179376b2240c2e
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-06-19 18:26:22 -0300

    req 5 - Done - adding find_duplicate func

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,28 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # raise NotImplementedError
+
+    try:
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            verify_array(nums[i])
+
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+
+        return False
+
+    except TypeError:
+        return False
+
+
+def verify_array(nums):
+    if nums < 0:
+        raise TypeError
+    if not isinstance(nums, int):
+        raise TypeError
+
+
+# teste_one = []
+# print(find_duplicate(teste_one))
